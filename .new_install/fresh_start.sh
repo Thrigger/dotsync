4d682ad75bbd0448cdb6e38947d97cfcffc0f32f
Thrigger <21341498+Thrigger@users.noreply.github.com>
2023-04-26 20:31:11 +0200

    Add applications file that lists all applications to be installed

--- .new_install/fresh_start.sh
+++ .new_install/fresh_start.sh
@@ -1,2 +1,8 @@
 # Assume superuser
-dnf install vim bat
+OPERATING_SYSTEM=$(lsb_release -d)
+if [[ $OPERATING_SYSTEM == *"Fedora"* ]]; then
+	INSTALL_APP="dnf -y"
+fi
+
+$INSTALL_APP upgrade
+$INSTALL_APP install ${ cat ~/scripts/applications.txt }
