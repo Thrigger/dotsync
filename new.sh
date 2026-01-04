d302717e2826824efee17612e51a0fbe527c9dba
Thrigger <21341498+Thrigger@users.noreply.github.com>
2026-01-04 22:40:52 +0100

    Add rust & starship install

--- new.sh
+++ new.sh
@@ -1,2 +1,15 @@
 sudo dnf install -y ansible
 ansible-playbook new.yaml
+
+# Rust
+if ! grep -q cargo ~/.bashrc
+then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+# Install Starship
+if ! grep -q starship ~/.bashrc
+then
+  cargo install starship
+  echo 'eval "$(starship init bash)"' >> ~/.bashrc
+fi
