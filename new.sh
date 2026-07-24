b9c7509a81fc5e1d9f145ab183f763a085ef3a9c
Thrigger <21341498+Thrigger@users.noreply.github.com>
2026-07-24 11:45:29 +0200

    !fixup 4b02611098bdf935cab7aa599cf8b19eda15109c

--- new.sh
+++ new.sh
@@ -6,6 +6,23 @@ if ! grep -q cargo ~/.bashrc; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
+# Sway
+if [ ! -f ~/.config/sway/config ]; then
+  echo "Copying default sway config from /etc/sway/config"
+  mkdir ~/.config/sway
+  cp /etc/sway/config ~/.config/sway/config
+fi
+if ! grep -q "Thrigger keyboard" ~/.config/sway/config; then
+  echo '# Thrigger keyboard\ninput "type:keyboard" xkb_layout se' >> ~/.config/sway/config
+fi
+
+# Kanshi
+if [ ! -f ~/.config/kanshi/config ]; then
+  echo "Creating empty Kanshi config - See README for instructions to set it up"
+  mkdir ~/.config/kanshi
+  touch ~/.config/kanshi/config
+fi
+
 # Starship
 if ! grep -q starship ~/.bashrc; then
   cargo install starship
