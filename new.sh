baaf91eab3a257d9d4ae7a0dee103b98a6edd089
Thrigger <21341498+Thrigger@users.noreply.github.com>
2026-07-24 11:33:14 +0200

    !fixup  d302717e2826824efee17612e51a0fbe527c9dba

--- new.sh
+++ new.sh
@@ -2,14 +2,12 @@ sudo dnf install -y ansible
 ansible-playbook new.yaml
 
 # Rust
-if ! grep -q cargo ~/.bashrc
-then
+if ! grep -q cargo ~/.bashrc; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-# Install Starship
-if ! grep -q starship ~/.bashrc
-then
+# Starship
+if ! grep -q starship ~/.bashrc; then
   cargo install starship
   echo 'eval "$(starship init bash)"' >> ~/.bashrc
 fi
